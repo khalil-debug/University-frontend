7da092a4481cfe34cad428bb635074e7dd53aa56
khalil <khaliltrigu@gmail.com>
2021-12-07 17:04:10 +0100

    master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EtudiantListComponent } from './etudiant-list/etudiant-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EtudiantListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/etudiant.ts
@@ -0,0 +1,7 @@
+export class Etudiant {
+    numInscription:number;
+    adresseETU:string;
+    nameETU:string;
+    prenomETU:string;
+    dateEntree:Date;
+}
